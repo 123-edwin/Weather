4574cbf107690ae317556a3276e89a3c1ef57d5e
Edwin Rios <54750644+123-edwin@users.noreply.github.com>
2024-10-25 17:28:07 +0000

    Cambio en viteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Weather/",
   plugins: [react()],
+  base: 'https://123-edwin.github.io/Weather',
 })
